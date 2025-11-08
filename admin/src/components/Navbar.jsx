35dde0d124ec7a31a4ac514a1e81e57a8f402917
siddhanta sahoo <siddhanta027z@gmail.com>
2025-11-08 09:34:57 +0530

    ..

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
       <div className="flex items-center gap-2 text-xs">
         <img
           className="w-36 sm::w-40 cursor-pointer"
-          src={assets.admin_logo}
+          src={assets.logoo}
           alt=""
         />
         <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
