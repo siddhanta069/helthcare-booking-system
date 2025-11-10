4906d6008fe528ef192ddeb9c73262de4d0d8bb5
siddhanta sahoo <siddhanta027z@gmail.com>
2025-11-10 07:57:26 +0530

    ...

--- clientside/src/context/AppContext.jsx
+++ clientside/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.API_BASE_URL;
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
